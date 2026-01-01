8dd0380bb0df69c7885e4263358775ee285b16c1
Dasari Aravind <aravinddasari7655@gmail.com>
2026-01-01 23:49:45 +0530

    Add files via upload

---
+++ Day1.sql
@@ -0,0 +1,36 @@
+/* Database
+A database is a collection of related data stored in tables, 
+where data is organized into rows and columns so computers can quickly find and work with it.*/
+
+/* What is SQL?
+
+SQL (Structured Query Language) is a language used to communicate with databases.
+
+You use SQL to:
+Retrieve data
+Insert data
+Update data
+Delete data
+Analyze data
+*/
+
+/* What is MySQL?
+
+MySQL is a relational database management system (RDBMS).
+
+It is software that:
+
+Stores data in tables
+Understands SQL
+Executes SQL queries
+Manages users, security, performance */
+
+show databases; -- To see available databases
+
+use animals; -- To use a specific database
+
+-- A table in a database is a structured way to store data in rows and columns.
+
+show tables; -- To see available Tables in thatdatabase
+
+select * from people; -- Selecting all the data from people's table
